f1c64b27c57a93a0b288b88ac08eefed0c891029
KIQVinicius <ksoares035@gmail.com>
2021-08-05 22:29:14 -0300

    refatoração

---
+++ Treino/preparacao.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import numpy as np
+from itertools import chain
+
+
+genero_musical ={
+    'ROCK': 0,
+    'SERTANEJO' : 1,
+    'FORRÓ': 2,
+    'GOSPEL' : 3,
+    'POP' : 4,
+    'FUNK' : 5,
+    'RAP' : 6
+}
+
+comida_favorita = {
+    'CHURRASCO' : 0,
+    'CASEIRA' : 1,
+    'VEGETARIANA' : 2,
+    'FAST FOOD' : 3,
+    'JAPONESA' : 4,
+    'ITALIANA' : 5
+}
+
+filme_favorito = {
+    'DRAMA' : 0,
+    'AÇÃO' : 1,
+    'AVENTURA' : 2,
+    'ROMANCE' : 3,
+    'ANIMAÇÃO' : 4,
+    'SUSPENSE' : 5,
+    'TERROR' : 6,
+    'COMÉDIA' : 7
+}
+
+esporte_favorito = {
+    'FUTEBOL' : 0,
+    'BASQUETE' : 1,
+    'VOLEI' : 2,
+    'TÊNIS' : 3,
+    'LUTAS' : 4
+}
+
+time = {
+    'PALMEIRAS' : 0,
+    'CORINTHIANS' : 1,
+    'SANTOS' : 2,
+    'SÃO PAULO' : 3,
+    'NENHUM' : 4
+}
+
+religiao = {
+    'CRISTIANISMO' : 0,
+    'JUDÁISMO' : 1,
+    'HINDUÍSMO' : 2,
+    'BUDISMO' : 3,
+    'ESPIRITISMO' : 4,
+    'NENHUMA' : 5
+}
+
+tem_filhos = {
+    'SIM' : 1,
+    'NÃO' : 0
+}
+
+#Substitui os valores pelo respectivo código
+def PadronizarValores(dataset):
+    dataset['genero_musical'] = pd.to_numeric(dataset['genero_musical'].str.upper().replace(genero_musical), errors='coerce').fillna(7)
+    dataset['comida_favorita'] = pd.to_numeric(dataset['comida_favorita'].str.upper().replace(comida_favorita), errors='coerce').fillna(6)
+    dataset['filme_favorito'] = pd.to_numeric(dataset['filme_favorito'].str.upper().replace(filme_favorito), errors='coerce').fillna(8)
+    dataset['esporte_favorito'] = pd.to_numeric(dataset['esporte_favorito'].str.upper().replace(esporte_favorito), errors='coerce').fillna(7)
+    dataset['time'] = pd.to_numeric(dataset['time'].str.upper().replace(time), errors='coerce').fillna(5)
+    dataset['religiao'] = pd.to_numeric(dataset['religiao'].str.upper().replace(religiao), errors='coerce').fillna(6)
+    dataset['tem_filhos'] = pd.to_numeric(dataset['tem_filhos'].str.upper().replace(tem_filhos), errors='coerce').fillna(3)
+    return dataset
+
+#Calcular idade
+def CalcularIdade(dataset ):
+    dataset['data_nascimento'] = dataset['data_nascimento'].replace('09/12/1377', '09/12/2000')
+    dataset['data_nascimento'] = dataset['data_nascimento'] + ' 00:00:00'
+    dataset['data_nascimento'] = pd.to_datetime(dataset['data_nascimento'], format='%d/%m/%Y %H:%M:%S')
+    dataset['idade'] = 2021 - dataset['data_nascimento'].dt.year
+    for index, row in dataset.iterrows():
+      if dataset['idade'][index] > 60 or dataset['idade'][index] < 10:
+        dataset['idade'][index] = dataset['idade'].mean()
+    return dataset
+
+def SepararDestinos(dataset):
+    for index, row in dataset.iterrows():
+        dataset['destino'][index] = row['destino'].replace('Jogos (kart, boliche, paintball...)', 'Jogos').strip()
+
+    auxDataset = dataset['destino'].str.split(',').astype(np.object)
+
+    lens = auxDataset.str.len()
+
+    dataset = pd.DataFrame({
+        'genero_musical' : dataset['genero_musical'].values.repeat(lens),
+        'comida_favorita' : dataset['comida_favorita'].values.repeat(lens),
+        'filme_favorito' : dataset['filme_favorito'].values.repeat(lens),
+        'esporte_favorito' : dataset['esporte_favorito'].values.repeat(lens),
+        'time' : dataset['time'].values.repeat(lens),
+        'religiao' : dataset['religiao'].values.repeat(lens),
+        'tem_filhos' : dataset['tem_filhos'].values.repeat(lens),
+        'idade' : dataset['idade'].values.repeat(lens),
+        'destino' : list(chain.from_iterable(auxDataset.tolist())), 
+    })
+    
+    locais = ['CINEMA', 'RESTAURANTE', 'SHOPPING', 'PARQUE', 'SHOW', 'MUSEU', 'BIBLIOTECA', 'ESTÁDIO', 'BIBLIOTECA', 'JOGOS', 'TEATRO', 'BAR']
+
+    for index, row in dataset.iterrows():
+        if dataset['destino'][index].strip().upper() not in locais:
+            print(dataset['destino'][index].upper())
+            dataset['destino'][index] = 'OUTROS'
+
+    return dataset
---
+++ Treino/treino_modelo.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+from scipy.sparse import data
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import export_graphviz
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.model_selection import GridSearchCV
+import preparacao
+from sklearn.ensemble import RandomForestClassifier
+
+dataset = pd.read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vTovXh_UG_XTyZoRjvsWsqOdLWpfLTxTLTG0_tj57dPg1AXlOMuqOsnezGQv8_PAQPnku8S-Nf-6PS4/pub?output=csv")
+
+dataset = dataset.rename(columns={'nome': 'nome_completo'})
+
+#Renomear colunas
+colunas = {
+    'Qual gênero musical você mais gosta?' : 'genero_musical',
+    'Qual o seu tipo de comida favorito?' : 'comida_favorita',
+    'Qual o seu estilo de filme favorito' : 'filme_favorito',
+    'Qual seu esporte favorito? ' : 'esporte_favorito',
+    'Torce para algum time?' : 'time',
+    'Possui alguma religião?' : 'religiao',
+    'Tem filhos?' : 'tem_filhos',
+    'Qual a sua data de nascimento? ' : 'data_nascimento',
+    'Que tipo de lugar você mais gosta de ir?' : 'destino'
+}
+
+dataset = dataset.rename(columns = colunas)
+
+dataset = preparacao.PadronizarValores(dataset)
+
+dataset = preparacao.CalcularIdade(dataset)
+
+print(dataset)
+
+dataset = preparacao.SepararDestinos(dataset)
+
+
+#Separação de treino e teste
+y = dataset['destino'].str.strip()
+x = dataset.drop(columns=['destino'])
+
+
+#Clusterização
+modelo = AgglomerativeClustering(n_clusters=3)
+grupos = modelo.fit_predict(x)
+x['Cluster'] = modelo.labels_
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+
+#Teste KNN
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+resultado = knn.predict(X_test)
+print (pd.crosstab(y_test,resultado, rownames=['Real'], colnames=['Predito'], margins=True))
+
+k_list = list(range(1,31))
+parametros = dict(n_neighbors=k_list)
+grid = GridSearchCV(knn, parametros, cv=5, scoring='accuracy')
+grid.fit(x,y)
+
+print("Melhores parametros {} com o valor de acurácia {} ".format(grid.best_params_,grid.best_score_))
+
+
+
+print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(X_train), len(X_test)))
+
+modelo = RandomForestClassifier()
+modelo.fit(X_train, y_train)
+previsoes_SVC = modelo.predict(X_train)
+acuracia = accuracy_score(y_train, previsoes_SVC) * 100
+print("A acurácia foi de %.2f%%" % acuracia)
+
