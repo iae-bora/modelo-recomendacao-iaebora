b2f1ba1a93ac8756419bd1cf5825979f02bc5379
KIQVinicius <ksoares035@gmail.com>
2021-09-13 22:51:54 -0300

    V1

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import pickle, os
 from numpy.core.records import record
+from scipy.sparse import data
 import treino_modelo as treino
 import random
 import pandas as pd
@@ -14,10 +15,38 @@ from queue import Queue
 import warnings
 warnings.filterwarnings('ignore')
 
-
 rodadas = []
 acuracias = []
+acuracias_sample = []
 retornos = []
+locais = ['CINEMA', 'RESTAURANTE', 'SHOPPING', 'PARQUE', 'SHOW', 'MUSEU', 'BIBLIOTECA', 'ESTÁDIO', 'BIBLIOTECA', 'JOGOS', 'TEATRO', 'BAR']
+
+
+enum_saida = {
+    "Cinema" : 0,
+    "Restaurante" : 1,
+    "Parque" : 2,
+    "Shopping" : 3,
+    "Show" : 4,
+    "Museu" : 5,
+    "Biblioteca" : 6,
+    "Estádio" : 7,
+    "Jogos" : 8,
+    "Teatro" : 9,
+    "Bar" : 10 
+}
+
+entrada = {
+    "musica" : 1,
+    "comida" : 1,
+    "filme" : 1,
+    "esporte": 1,
+    "time" : 1,
+    "religiao" : 1,
+    "filhos" : 1,
+    "nascimento" : 20, 
+    "qtd_destinos" : 2
+}
 
 app = Flask(__name__)
 CORS(app)
@@ -28,39 +57,46 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    entrada = request.get_json(force=True)['answers']
+    #entrada = request.get_json(force=True)['answers']
+
+    dataset = pd.read_csv("dados.csv")
+    dataset = dataset.drop(columns='Unnamed: 0')
+    dataset = dataset.query("destino != 'OUTROS'")
 
     lista = list(entrada.values())
-   # print(lista)
-
-    locais = ['CINEMA', 'RESTAURANTE', 'SHOPPING', 'PARQUE', 'SHOW', 'MUSEU', 'BIBLIOTECA', 'ESTÁDIO', 'BIBLIOTECA', 'JOGOS', 'TEATRO', 'BAR']
+    print(lista)
 
     for i in range(entrada['qtd_destinos']):
-        modelo = treino.Recomendacao(locais)
-        recomendacao = modelo.predict([lista])
-        retornos.append(recomendacao[0].upper())
-        #print(recomendacao[0].upper())
-        if recomendacao[0].upper() != 'OUTROS':
-            locais.remove(recomendacao[0].upper())
-    saida = {'recomendacao' : retornos}
+        dataset = dataset[dataset['destino'].str.upper().isin(locais)]
+        modelo = treino.Recomendar(dataset, entrada)
+        recomendacao = modelo.predict([lista])[0]
+        recomendacao_convertida = enum_saida[recomendacao]
+        retornos.append(recomendacao_convertida)
 
-    return saida
+        if recomendacao.upper() != 'OUTROS':
+            locais.remove(recomendacao.upper())
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    return retornos
 
-def gerarBase():
-    locais = ['CINEMA', 'RESTAURANTE', 'SHOPPING', 'PARQUE', 'SHOW', 'MUSEU', 'BIBLIOTECA', 'ESTÁDIO', 'BIBLIOTECA', 'JOGOS', 'TEATRO', 'BAR']
-    dataset = pd.read_csv("C:\\Users\\Kaique\\OneDrive\\Área de Trabalho\\TCC\dados1.csv")
+
+@app.route('/Train', methods=['POST'])
+def Train():
+    dataset = pd.read_csv("dados_treino.csv")
     dataset = dataset.drop(columns='Unnamed: 0')
+    dataset = dataset.query("destino != 'OUTROS'")
+    sample = dataset.sample(n=50)
+    print(dataset.shape)
+    print(dataset)
+    dataset = dataset[~dataset.isin(sample)]
+    dataset = dataset.dropna()
 
-    #plt.ion()
-    for rodada in range(6000):
+    plt.ion()
+    for rodada in range(20000):
         
         entrada = [random.randint(0,6), random.randint(0,5), random.randint(0,7), random.randint(0,4), random.randint(0,4), random.randint(0,5), random.randint(0,1), random.randint(15,60)]
-        resposta, acuracia  = treino.Recomendacao(locais, dataset, rodada, entrada)
+        resposta, acuracia, acuracia_sample = treino.Treinar(locais, dataset, rodada, entrada, sample)
         acuracias.append(acuracia)
+
         rodadas.append(rodada)
         entrada.append(str(resposta[0]))
 
@@ -76,18 +112,20 @@ def gerarBase():
             'destino' : entrada[9]
         }
 
+
         dataset = dataset.append(dict_entrada, ignore_index=True)
 
-        if rodada == 5999:
+        if rodada == 19999:
             plt.plot(rodadas, acuracias)
             plt.xlabel("Rodada de treino")
             plt.ylabel("Acurácia")
             plt.draw()
+            plt.pause(0.05)
             plt.show()
     return dataset
 
 
 
-if __name__ == '__main__':
-    dataset = gerarBase()
-    dataset.to_csv("C:\\Users\\Kaique\\OneDrive\\Área de Trabalho\\TCC\\dados.csv")
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
--- treino_modelo.py
+++ treino_modelo.py
@@ -1,3 +1,4 @@
+from numpy.core.fromnumeric import mean
 import pandas as pd
 import numpy as np
 from scipy.sparse import data
@@ -16,42 +17,78 @@ import preparacao
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import BaggingClassifier
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import silhouette_score
 bagging = BaggingClassifier(KNeighborsClassifier(), max_samples=0.5, max_features=0.5)
-
+from sklearn.model_selection import cross_validate
 from sklearn.ensemble import RandomForestClassifier
 import warnings
 warnings.filterwarnings('ignore')
 
 
-def Recomendacao(locais, dataset, rodada, entrada):
-
-
+def Treinar(locais, dataset, rodada, entrada, sample):
     #Separação de treino e teste
     #dataset.to_csv('C:\\Users\\Kaique\\OneDrive\\Área de Trabalho\\TCC\\dados1.csv')
     y = dataset['destino'].str.strip()
     x = dataset.drop(columns=['destino'])
-
+    #y_sample = sample['destino'].str.strip()
+    #x_sample = sample.drop(columns=['destino'])
 
     #Clusterização
-    modeloCluster = KMeans(n_clusters=3)
-    grupos = modeloCluster.fit_predict(x)
-    x['Cluster'] = modeloCluster.labels_
-    entrada.append(modeloCluster.predict([entrada]))
+    kmeans = KMeans(n_clusters=3)
+    #grupos = kmeans.fit_predict(x)
+    #x['Cluster'] = kmeans.labels_
+
+    data_array = x.values
+    #kmeans = KMeans(n_clusters=5, init='k-means++', n_init=10)
+    x["clusters"] = kmeans.fit_predict(data_array)
+    #plot_kmeans = x.groupby("clusters").aggregate("mean").plot.bar()
+    entrada.append(kmeans.predict([entrada]))
 
 
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, stratify=y)
 
+
+    modelo = DecisionTreeClassifier()
+    modelo.fit(X_train, y_train)
+
+
+    results = cross_validate(modelo, x, y, cv = 3, n_jobs = 3)
+    previsoes_SVC = modelo.predict(X_train)
+    acuracia = accuracy_score(y_train, previsoes_SVC) * 100
+    acuracia_cross = mean(results['test_score'])*100
+    
+    #previsoes_sample = modelo.predict(x_sample)
+    #acuracia_sample = accuracy_score(y_sample, previsoes_sample) * 100
+
+    print("---------------------------------------------------------------------------------------------")
+    print("Rodada: " + str(rodada))
+    print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(X_train), len(X_test)))
+    print("A acurácia foi de %.2f%%" % acuracia)
+    print("A acurácia cross foi de %.2f%%" % acuracia_cross)
+    #print(dataset['destino'].value_counts)
+    saida = modelo.predict([entrada])
+
+    return saida, acuracia, ""
+
+def Recomendar(dataset, resposta):
+    #Clusterização
+
+    y = dataset['destino'].str.strip()
+    x = dataset.drop(columns=['destino'])
+
+    kmeans = KMeans(n_clusters=3)
+    x['Cluster'] = kmeans.fit_predict(x)
+
+    
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, stratify=y)
 
     modelo = DecisionTreeClassifier()
     modelo.fit(X_train, y_train)
     previsoes_SVC = modelo.predict(X_train)
     acuracia = accuracy_score(y_train, previsoes_SVC) * 100
-    print("---------------------------------------------------------------------------------------------")
-    print("Rodada: " + str(rodada))
-    print("Treinaremos com %d elementos e testaremos com %d elementos" % (len(X_train), len(X_test)))
     print("A acurácia foi de %.2f%%" % acuracia)
-    #print(dataset['destino'].value_counts)
-    saida = modelo.predict([entrada])
+    #print(x['destino'].value_counts)
 
-    return saida, acuracia
+    return modelo
 
